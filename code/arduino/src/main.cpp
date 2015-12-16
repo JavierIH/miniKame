70194c963e6373da5ab2bcd88952e96797523b03
JavierIH <javier.i.h@hotmail.com>
2015-12-16 11:22:51 +0100

    walk and home functions

--- code/arduino/src/main.cpp
+++ code/arduino/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <ESP8266mDNS.h>
 #include <Servo.h>
 #include "octosnake.h"
 
@@ -9,7 +10,7 @@ const char* password = "password";
 Oscillator oscillator[8];
 Servo servo[8];
 int board_pins[] = {D1, D4, D8, D6, D7, D5, D2, D3};
-int trim[] = {1, -8, 6, 4, 2, -5, 6, 2};
+int trim[] = {1, -8, 8, 4, 2, -5, 6, 2};
 
 int angToUsec(float value){
     int usecs = value/180 * (MAX_PULSE_WIDTH-MIN_PULSE_WIDTH) + MIN_PULSE_WIDTH;
@@ -28,7 +29,7 @@ void execute(float steps, float period[8], int amplitude[8], int offset[8], int 
     while (millis() < final){
         for (int i=0; i<8; i++){
             servo[i].writeMicroseconds(angToUsec(oscillator[i].refresh()));
-            Serial.println(i);
+            Serial.println("");
         }
     }
 }
@@ -95,22 +96,73 @@ void run(float steps, float T=5000){
     execute(steps, period, amplitude, offset, phase);
 }
 
+void walk(float steps, float T=5000){
+    int x_amp = 15;
+    int z_amp = 15;
+    int ap = 20;
+    int hi = 15;
+    int front_x = 12;
+    float period[] = {T, T, T/2, T/2, T, T, T/2, T/2};
+    int amplitude[] = {x_amp,x_amp,z_amp,z_amp,x_amp,x_amp,z_amp,z_amp};
+    int offset[] = {    90+ap-front_x,
+                        90-ap+front_x,
+                        90-hi,
+                        90+hi,
+                        90-ap-front_x,
+                        90+ap+front_x,
+                        90+hi,
+                        90-hi
+                    };
+    int  phase[] = {90, 90, 270, 90, 270, 270, 90, 270};
+
+    for (int i=0; i<8; i++){
+        oscillator[i].reset();
+        oscillator[i].setPeriod(period[i]);
+        oscillator[i].setAmplitude(amplitude[i]);
+        oscillator[i].setPhase(phase[i]);
+        oscillator[i].setOffset(offset[i]);
+    }
+
+    unsigned long final = millis() + period[0]*steps;
+    unsigned long init = millis();
+    while (millis() < final){
+        int side = (int)((millis()-init) / (period[0]/2)) % 2;
+        servo[0].writeMicroseconds(angToUsec(oscillator[0].refresh()));
+        servo[1].writeMicroseconds(angToUsec(oscillator[1].refresh()));
+        servo[4].writeMicroseconds(angToUsec(oscillator[4].refresh()));
+        servo[5].writeMicroseconds(angToUsec(oscillator[5].refresh()));
+        if (side == 0){
+            servo[3].writeMicroseconds(angToUsec(oscillator[3].refresh()));
+            servo[6].writeMicroseconds(angToUsec(oscillator[6].refresh()));
+        }
+        else{
+            servo[2].writeMicroseconds(angToUsec(oscillator[2].refresh()));
+            servo[7].writeMicroseconds(angToUsec(oscillator[7].refresh()));
+        }
+    }
+}
+
+void home(){
+    int ap = 20;
+    int hi = 35;
+    int position[] = {90+ap,90-ap,90-hi,90+hi,90-ap,90+ap,90+hi,90-hi};
+    for (int i=0; i<8; i++) servo[i].writeMicroseconds(angToUsec(position[i])+trim[i]);
+}
+
+void zero(){
+    for (int i=0; i<8; i++) servo[i].writeMicroseconds(angToUsec(90+trim[i]));
+}
+
 void setup(){
     Serial.begin(115200);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-    }
     for(int i=0; i<8; i++) servo[i].attach(board_pins[i]);
     for(int i=0; i<8; i++) oscillator[i].setTrim(trim[i]);
 }
 
 void loop(){
-    //for (int i=0; i<8; i++)
-    //    servo[i].writeMicroseconds(angToUsec(90+trim[i]));
-
-    //run(10, 850);
-    turnR(10, 600);
-    turnL(10, 600);
+    zero();
     delay(5000);
+    home();
+    delay(5000);
+    //walk(2, 250);
 }
